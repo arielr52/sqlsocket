19948eb9fe55c5b2e37c3e19e3b861254d2e6a6c
venkat <venkat.bitla@gmail.com>
2015-05-17 16:17:24 +0530

    initial node.js server commit

---
+++ node/index.js
@@ -0,0 +1,24 @@
+var Hapi = require('hapi');
+
+var server = new Hapi.Server();
+server.connection({ port: 3000 });
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});
+
+server.start(function () {
+    console.log('Server running at:', server.info.uri);
+});
